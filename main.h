5170bf964617bae067ef3847ebc3da95b43cc73e
Islamiat <islamiatokedele19@gmail.com>
2022-08-08 07:03:11 -0700

    printalpha

--- main.h
+++ main.h
@@ -24,7 +24,6 @@ typedef struct flags
  * @c: format specifier
  * @f: pointer to the correct printing function
  */
-
 typedef struct printHandler
 {
 	char c;
@@ -52,7 +51,6 @@ int _printf(const char *format, ...);
 /* get_print */
 int (*get_print(char s))(va_list, flags_t *);
 
-
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
@@ -63,7 +61,6 @@ int print_char(va_list l, flags_t *f);
 /* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
-
 
 /* print_custom */
 int print_rot13(va_list l, flags_t *f);
